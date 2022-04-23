5dd7d07355659e1ab6e6bbe2bd2912e5d1e6f65d
mohamedmowafy <64433872+mohamed-mowafya@users.noreply.github.com>
2022-04-23 12:20:20 -0400

    auth update + cleanup

--- myapp/app.js
+++ myapp/app.js
@@ -3,13 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require("cors")
 var passport = require('passport')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+
 var sanitize = require('mongo-sanitize');
 var User = require('./models/user')
+const bodyParser = require("body-parser")
 
 var app = express();
 var mongoose = require('mongoose');
@@ -18,124 +21,80 @@ const dotenv = require('dotenv');
 const helmet = require('helmet')
 const session = require('express-session')
 const localStrategy = require('passport-local').Strategy // Auth strategy.
-const bcrypt = require('bcrypt')
-
+const bcrypt = require('bcryptjs')
 dotenv.config()
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '/public')));
-app.use(session({
-  secret:process.env.SESS_SECRET,
-  resave: false,
-  saveUninitialized: true
-}))
-
-app.use(helmet())
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-
-passport.serializeUser(function (user,done){
-  done(null, user.id)
-});
-
-passport.deserializeUser((id,done)=>{
-  User.findById(id, (err,user) =>{
-    done(err,user)
-  })
-});
-
-passport.use(new localStrategy((email,password,done)=>{
-  User.findOne({email:email}, (err,user)=>{
-    if(err) return done(err);
-    if(!user){
-      return done(null,false,{message: 'Incorrect Username'})
-    }
-
-    bcrypt.compare(password,user.password,(err,res)=>{
-      if(err){
-        return done(error)
-      }
-      if(res === false){
-        return done(null,false,{message:'Incorrect Pasword'})
-      }
-      return done(null,user)
-    })
-  })
-}))
-
-// Setup admin user
-app.post('/register', async (req, res) => {
-	const exists = await User.exists({ email: req.body.email });
-
-
-	bcrypt.genSalt(10, function (err, salt) {
-		if (err) return next(err);
-		bcrypt.hash(req.body.pass, salt, function (err, hash) {
-			if (err) return next(err);
-			
-			const newUser = new User({
-				email: req.body.email,
-				password: hash
-			});
-
-			newUser.save();
-
-		});
-	});
-  res.send(JSON.stringify({message:"User Registered"}))
-});
-
-function isLoggedIn(req, res, next) {
-	if (req.isAuthenticated()) return next();
-	res.redirect('/login');
-}
-
-function isLoggedOut(req, res, next) {
-	if (!req.isAuthenticated()) return next();
-	res.redirect('/');
-}
-
-
-
-app.listen(3000,()=>{
-  console.log("Listening on port 3000")
-})
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-
-
+//---------------------------IMPORTS AND CONFIGS---------------------------------------------------------------------------//
 
 mongoose.connect(process.env.MONGO_URI)
    .then(() =>  console.log('db connection successful'))
    .catch((err) => console.error(err));
 
 
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(session({
+  secret:process.env.SESS_SECRET,
+  resave: true,
+  saveUninitialized: true
+}))
+app.use(cors({
+  origin:"http://localhost:3000",
+  credentials:true
+}))
+app.use(cookieParser(process.env.SESS_SECRET));
+app.use(helmet())
+app.use(passport.initialize());
+app.use(passport.session());
+require("./controllers/passportConfig")(passport)
+
+
+//---------------------------------------MIDDLEWARES----------------------------//
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+// Routes
+app.post("/login", (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) throw err;
+    if (!user) res.send("No User Exists");
+    else {
+      req.logIn(user, (err) => {
+        if (err) throw err;
+        res.send("Successfully Authenticated");
+        console.log(req.user);
+      });
+    }
+  })(req, res, next);
+});
+
+app.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }, async (err, doc) => {
+    if (err) throw err;
+    if (doc) res.send("User Already Exists");``
+    if (!doc) {
+      const hashedPassword = await bcrypt.hash(req.body.password,10); // Bcrypt has with salt
+
+      const newUser = new User({
+        email: req.body.email,
+        password: hashedPassword,
+      });
+      await newUser.save();
+      res.send("User Created");
+    }
+  });
+});
+
+app.get("/user", (req, res) => {
+  res.send(req.user); 
+});
+
+//// -------------- END OF ROUTES------------------///
+app.listen(3000,()=>{
+  console.log("Listening on port 3000")
+})
 
 
 module.exports = app;
