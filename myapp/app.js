cb2e3ae3191deabf7399d50d8945c355f79fa1fc
mohamedmowafy <64433872+mohamed-mowafya@users.noreply.github.com>
2022-04-23 15:12:24 -0400

    Login auth fix

--- myapp/app.js
+++ myapp/app.js
@@ -1,36 +1,34 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const cors = require("cors")
-var passport = require('passport')
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-
-var sanitize = require('mongo-sanitize');
-var User = require('./models/user')
-const bodyParser = require("body-parser")
-
-var app = express();
-var mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+const passport = require("passport");
+const passportLocal = require("passport-local").Strategy;
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcryptjs");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const app = express();
+const User = require("./models/user");
+const path = require('path');
 const dotenv = require('dotenv');
 const helmet = require('helmet')
-const session = require('express-session')
-const localStrategy = require('passport-local').Strategy // Auth strategy.
-const bcrypt = require('bcryptjs')
+var sanitize = require('mongo-sanitize');
 dotenv.config()
 //---------------------------IMPORTS AND CONFIGS---------------------------------------------------------------------------//
 
-mongoose.connect(process.env.MONGO_URI)
-   .then(() =>  console.log('db connection successful'))
-   .catch((err) => console.error(err));
+mongoose.connect(
+  process.env.MONGO_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("Mongoose Is Connected");
+  }
+);
 
 
-app.use(logger('dev'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/public')));
@@ -40,9 +38,10 @@ app.use(session({
   saveUninitialized: true
 }))
 app.use(cors({
-  origin:"http://localhost:3000",
-  credentials:true
-}))
+  origin: "http://localhost:3000", // <-- location of the react app were connecting to
+  credentials: true,
+}
+))
 app.use(cookieParser(process.env.SESS_SECRET));
 app.use(helmet())
 app.use(passport.initialize());
@@ -50,16 +49,14 @@ app.use(passport.session());
 require("./controllers/passportConfig")(passport)
 
 
-//---------------------------------------MIDDLEWARES----------------------------//
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 
-// Routes
+//-----------------ROUTES--------------------------------------------------------//
+
 app.post("/login", (req, res, next) => {
   passport.authenticate("local", (err, user, info) => {
     if (err) throw err;
-    if (!user) res.send("No User Exists");
+    if (!user) res.send("Auth error");
     else {
       req.logIn(user, (err) => {
         if (err) throw err;
@@ -70,12 +67,13 @@ app.post("/login", (req, res, next) => {
   })(req, res, next);
 });
 
+
 app.post("/register", (req, res) => {
   User.findOne({ email: req.body.email }, async (err, doc) => {
     if (err) throw err;
-    if (doc) res.send("User Already Exists");``
+    if (doc) res.send("User Already Exists");
     if (!doc) {
-      const hashedPassword = await bcrypt.hash(req.body.password,10); // Bcrypt has with salt
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
       const newUser = new User({
         email: req.body.email,
@@ -91,9 +89,10 @@ app.get("/user", (req, res) => {
   res.send(req.user); 
 });
 
+
 //// -------------- END OF ROUTES------------------///
-app.listen(3000,()=>{
-  console.log("Listening on port 3000")
+app.listen(5000,()=>{
+  console.log("Listening on port 5000")
 })
 
 
--- myapp/controllers/passportConfig.js
+++ myapp/controllers/passportConfig.js
@@ -2,38 +2,37 @@ const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 const localStrategy = require("passport-local").Strategy;
 
-
-
 module.exports = function (passport) {
-    passport.use(
-      new localStrategy((email, password, done) => {
-        User.findOne({ email: email }, (err, user) => {
+  passport.use(
+    new localStrategy({
+      usernameField: "email",
+      passwordField: "password"
+    },
+      (username, password, done) => {
+      User.findOne({ email: username }, (err, user) => {
+        if (err) throw err;
+        if (!user) return done(null, false);
+        bcrypt.compare(password, user.password, (err, result) => {
           if (err) throw err;
-          if (!user) return done(null, false);
-          bcrypt.compare(password, user.password, (err, result) => {
-            if (err) throw err;
-            if (result === true) {
-              return done(null, user);
-            } else {
-              return done(null, false);
-            }
-          });
+          if (result === true) {
+            return done(null, user);
+          } else {
+            return done(null, false);
+          }
         });
-      })
-    );
-
-
-
-passport.serializeUser(function (user,done){
-    done(null, user.id)
-  });
-  
-  passport.deserializeUser((id,done)=>{
-    User.findOne({ _id: id }, (err,user) =>{
-        const userInformation = {
-            email: user.email,
-          };
-      done(err,userInformation)
+      });
     })
+  );
+
+  passport.serializeUser((user, cb) => {
+    cb(null, user.id);
+  });
+  passport.deserializeUser((id, cb) => {
+    User.findOne({ _id: id }, (err, user) => {
+      const userInformation = {
+        email: user.email,
+      };
+      cb(err, userInformation);
+    });
   });
 };
