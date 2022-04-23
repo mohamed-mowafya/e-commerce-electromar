3e829005a3be564e74bdb112ecf2088d94541e14
mohamedmowafy <64433872+mohamed-mowafya@users.noreply.github.com>
2022-04-23 18:48:11 -0400

    separated logic into controller and route files

--- myapp/app.js
+++ myapp/app.js
@@ -4,16 +4,16 @@ const cors = require("cors");
 const passport = require("passport");
 const passportLocal = require("passport-local").Strategy;
 const cookieParser = require("cookie-parser");
-const bcrypt = require("bcryptjs");
 const session = require("express-session");
 const bodyParser = require("body-parser");
 const app = express();
-const User = require("./models/user");
 const path = require('path');
 const dotenv = require('dotenv');
 const helmet = require('helmet')
-var sanitize = require('mongo-sanitize');
+const sanitize = require('mongo-sanitize');
 dotenv.config()
+
+const authRoutes = require('./routes/userAuthRoutes')
 //---------------------------IMPORTS AND CONFIGS---------------------------------------------------------------------------//
 
 mongoose.connect(
@@ -50,47 +50,10 @@ require("./controllers/passportConfig")(passport)
 
 
 
-
 //-----------------ROUTES--------------------------------------------------------//
+app.use('/',authRoutes)
 
-app.post("/login", (req, res, next) => {
-  passport.authenticate("local", (err, user, info) => {
-    if (err) throw err;
-    if (!user) res.send("Auth error");
-    else {
-      req.logIn(user, (err) => {
-        if (err) throw err;
-        res.send("Successfully Authenticated");
-        console.log(req.user);
-      });
-    }
-  })(req, res, next);
-});
-
-
-app.post("/register", (req, res) => {
-  User.findOne({ email: req.body.email }, async (err, doc) => {
-    if (err) throw err;
-    if (doc) res.send("User Already Exists");
-    if (!doc) {
-      const hashedPassword = await bcrypt.hash(req.body.password, 10);
-
-      const newUser = new User({
-        email: req.body.email,
-        password: hashedPassword,
-      });
-      await newUser.save();
-      res.send("User Created");
-    }
-  });
-});
-
-app.get("/user", (req, res) => {
-  res.send(req.user); 
-});
-
-
-//// -------------- END OF ROUTES------------------///
+//// -------------- END OF ROUTES------------------------------------------------//
 app.listen(5000,()=>{
   console.log("Listening on port 5000")
 })
---
+++ myapp/controllers/userAuth.js
@@ -0,0 +1,36 @@
+const passport = require("passport");
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+
+const login = (req,res,next) =>{
+    passport.authenticate("local", (err, user, info) => {
+        if (err) throw err;
+        if (!user) res.send("Auth error");
+        else {
+          req.logIn(user, (err) => {
+            if (err) throw err;
+            res.send("Successfully Authenticated");
+            console.log(req.user);
+          });
+        }
+      })(req, res, next);
+}
+
+const signUp = (req,res,next) =>{
+    User.findOne({ email: req.body.email }, async (err, doc) => {
+        if (err) throw err;
+        if (doc) res.send("User Already Exists");
+        if (!doc) {
+          const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    
+          const newUser = new User({
+            email: req.body.email,
+            password: hashedPassword,
+          });
+          await newUser.save();
+          res.send("User Created");
+        }
+      });
+}
+
+module.exports = {login,signUp}
