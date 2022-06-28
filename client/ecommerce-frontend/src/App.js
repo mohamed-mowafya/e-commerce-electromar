ea0b96fe7b0d63c9f721a92ca5550c0d507ce76a
mohamedmowafy <64433872+mohamed-mowafya@users.noreply.github.com>
2022-06-28 14:32:24 -0400

    login + signup page

--- client/ecommerce-frontend/src/App.js
+++ client/ecommerce-frontend/src/App.js
@@ -2,9 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import React from 'react';
-import Login from './components/Login/Login';
 import Navbar from './components/Reuse/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Login from './components/Login/Login';
+import SignUp from './components/Login/Signup';
 
 function App() {
   return (
@@ -13,7 +15,7 @@ function App() {
   <Navbar/>
      <Routes>
     <Route path="/login" element={<Login/>}/>
-    <Route path="/signup" element={null}/>
+    <Route path="/signup" element={<SignUp/>}/>
      </Routes>
      </Router>
      </React.Fragment>
--- client/ecommerce-frontend/src/components/Login/Signup.js
+++ client/ecommerce-frontend/src/components/Login/Signup.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import classes from "./login.module.css"
 import {Link} from "react-router-dom"
 
-const Login = () =>{
+const SignUp = () =>{
 
 useEffect(()=>{
     
@@ -14,25 +14,28 @@ return(
     <div className="container p-5">
     <div className="row">
     <div className="col-md-6">
-    <h1 className={`mb-4 ${classes.loginTitle}`}>Sign in</h1>
+    <h1 className={`mb-4 ${classes.loginTitle}`}>Create an account</h1>
     <form className="w-auto">
-    <div className="col-sm-6 mb-3">
+    <div className={`col-sm-6 mb-3`}>
     <label for="exampleInputEmail1" className={`form-label ${classes.loginFormText}`}>Email address</label>
     <input type="email" className={`form-control`} id="exampleInputEmail1" aria-describedby="emailHelp"/>
     <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
   </div>
-  <div class="col-sm-6 mb-3">
+  <div class="col-sm-6 mb-3">   
     <label for="exampleInputPassword1" className={`form-label ${classes.loginFormText}`}>Password</label>
+    <input type="password" className={`form-control`} id="exampleInputPassword1"/>
+  </div>
+  <div class="col-sm-6 mb-3">
+    <label for="exampleInputPassword1" className={`form-label ${classes.loginFormText}`}>Confirm your Password</label>
     <input type="password" className={`form-control`} id="exampleInputPassword1"/>
   </div>
   <div className="col-sm-5">
    
-  <button style={{backgroundColor:"black"}} type="submit" className={`btn ps-4 pe-4 ${classes.loginBtn}`}><b>Sign in</b></button>
+  <button style={{backgroundColor:"black"}} type="submit" className={`btn ps-4 pe-4 ${classes.loginBtn}`}><b>Sign up</b></button>
   </div>
 </form>
 </div>
-<div className="col-md-6">
-    <h3 className={`${classes.secondSection}`}>Don't have an account?</h3>
+<div style={{marginTop:"10%"}} className="col-md-6">
 <p style={{color:'black'}}>Here are some of the benefits you’ll enjoy:</p>
 <div className="d-flex">
 <i style={{color:"red"}} className={`pi pi-shopping-cart ${classes.loginCartLogo}`}></i> 
@@ -44,7 +47,6 @@ return(
 <p className={classes.descriptionSection}>Quick Recap</p>
 </div>
 <p style={{color:'black',marginLeft:'1%'}}>Your order history is a click away.</p>
-    <Link className={classes.signUpLink} style={{marginLeft:'1%'}} to="/signup">Sign Up Today ></Link>
     </div>
 </div>
 
@@ -54,4 +56,4 @@ return(
 )
 }
 
-export default Login
+export default SignUp
