9c8578a5607565742d30b6ff249df60ce90921b9
mmoawfy-a <mowafymohamed.a@gmail.com>
2023-01-29 12:14:10 -0500

    Changed carousel for flickity library

--- client/src/components/Home/HomePage/HomePage.jsx
+++ client/src/components/Home/HomePage/HomePage.jsx
@@ -1,48 +1,35 @@
 import React, { useState, useEffect } from "react";
 import xbox from "../../../images/carousel/xbox.jpg";
 import iphone from "../../../images/carousel/iphone.jpg"
-import classes from "./homepage.module.css";
+import classNamees from "./homepage.module.css";
 import psvr2 from "../../../images/carousel/psvr2.jpg"
 import forspoken from "../../../images/carousel/forspoken.jpg"
+import "./flickity_modif.css";
+import Flickity from 'react-flickity-component';
 
 const HomePage = () => {
 
 
+    const flickityOptions = {
+        autoPlay: true,
+        wrapAround: true,
+        isSelected: true
+    }
+
+
     return (
-        <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
-            <div className="carousel-inner">
-                <div className="carousel-item active">
-                    <div>
-                        <div className={classes.carouselImgContainer}>
-                        <img src={forspoken} className="w-100" alt="PS5" />
-                        <span className={classes.carouselTextLeft}>FIND YOUR FIGHT</span>
-                        </div>
-                        
-                    </div>
-                </div>
-
-                <div className="carousel-item">
-                    <div>
-                        <img src={xbox} className="w-100" alt="PS5" />
-                    </div>
-                </div>
-
-                <div className="carousel-item w-100">
-                    <div>
-                        <img src={psvr2} className="w-100" alt="PS5" />
-                    </div>
-                </div>
-
-            </div>
-            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Previous</span>
-            </button>
-            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Next</span>
-            </button>
-        </div>
+        <Flickity
+            className={'carousel'} // default ''
+            elementType={'div'} // default 'div' 
+            options={flickityOptions}// takes flickity options {}
+            disableImagesLoaded={true} // default false
+            reloadOnUpdate // default false
+            static // default false
+        >
+            <img className="carousel-cell img-fluid" src={forspoken} />
+            <img className="carousel-cell img-fluid" src={xbox} />
+            <img className="carousel-cell img-fluid" src={psvr2} />
+        </Flickity>
     );
 };
 
