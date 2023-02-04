7754aa62a4cb80c97c7c6ef1d7bd4a9667190f6e
mmoawfy-a <mowafymohamed.a@gmail.com>
2023-02-04 12:20:25 -0500

    Added featured products carousels

--- client/src/components/Product/Product.jsx
+++ client/src/components/Product/Product.jsx
@@ -1,39 +1,73 @@
 import React from "react";
 import "./product_card.css"
 import reuseClasses from "../../components/Reusable/reuse.module.css";
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css';
+import splatoon from "../../images/products/splatoon.jpg"
+
 
 const Product = () => {
 
-    return (
-        <React.Fragment>
-            <section>
-                <div className="card mb-4 mt-3 ms-auto me-auto" style={{ width: "16rem"}}>
-                    <div className="bg-image hover-overlay ripple ripple-surface ripple-surface-light"
-                        data-mdb-ripple-color="light">
-                        <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Products/12.webp"
-                            className="img-fluid"
-                            alt="Laptop" />
-                        <a href="#!">
-                            <div className="mask"></div>
-                        </a>
-                    </div>
-                    <div className="card-body pb-0">
-                        <div className="d-flex justify-content-center">
-                            <div className="text-center">
-                            <b><p className={`text-dark ${reuseClasses.productTitle}`}>Dell Xtreme 270</p></b>
-                            <b><p className={`text-dark ${reuseClasses.productTitle}`}>333$</p></b>
-                            </div>
-
-                        </div>
-                    </div>
-                    <div className="card-body">
-                        <div className="d-flex justify-content-between align-items-center pb-0">
-                            <button type="button" className="btn btn-primary ms-auto me-auto">Add to cart</button>
-                        </div>
+    const Card = () => {
+        return (
+            <div className="card col-md-4 h-25 border-0 ms-auto me-auto">
+                <div className="bg-image hover-overlay ripple ripple-surface ripple-surface-light"
+                    data-mdb-ripple-color="light">
+                    <img src={splatoon} />
+                    <a href="#!">
+                        <div className="mask"></div>
+                    </a>
+                </div>
+                <div className="card-body p-0">
+                    <div className="text-left">
+                        <p className={`${reuseClasses.productTitle} pb-0`}>Dell Xtreme 270</p>
+                        <b><p className={`text-dark `}>333$</p></b>
                     </div>
                 </div>
+                <div className="card-body p-0 mt-2">
+                    <div className="d-flex justify-content-center">
+                        <i style={{ color: "red" }} className="pi pi-shopping-cart mt-1 pe-2 fw-bold" />
+                        <button type="button" className={`btn p-0 ${reuseClasses.cartBtn} fw-bold`}>Add to cart</button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 
-            </section>
+    return (
+        <React.Fragment>
+            <h3 className={`${reuseClasses.productText} text-dark mt-5 text-center`}>Featured Products</h3>
+            <div className="wrapper">
+                <h2 id="basic-example-heading">Basic Example</h2>
+
+                <Splide
+                    options={{
+                        perPage: 3,
+                        height: '30rem',
+                        rewind: true,
+                        gap: '0.5rem',
+                    }}
+                    aria-labelledby="basic-example-heading"
+                >
+                    <SplideSlide>
+                        <Card />
+                    </SplideSlide>
+                    <SplideSlide>
+                        <Card />
+                    </SplideSlide>
+                    <SplideSlide>
+                        <Card />
+                    </SplideSlide>
+                    <SplideSlide>
+                        <Card />
+                    </SplideSlide>
+                    <SplideSlide>
+                        <Card />
+                    </SplideSlide>
+                    
+                </Splide>
+            </div>
+
         </React.Fragment>
     );
 
